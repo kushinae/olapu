86df844668f0b78ac45b746f8768a166f84ada4d
bnyte <bnytezz@gmail.com>
2023-02-15 01:19:45 +0800

    feat: 完善代码补全

--- olapu-ui/src/extensions/editor/EditorExtension.ts
+++ olapu-ui/src/extensions/editor/EditorExtension.ts
@@ -1,50 +1,14 @@
 import { IExtension } from "@dtinsight/molecule/esm/model";
-import { languages, editor } from '@dtinsight/molecule/esm/monaco';
+import { languages } from '@dtinsight/molecule/esm/monaco';
 import { IExtensionService } from "@dtinsight/molecule/esm/services";
+import {Keywords, Snippets} from "@/utils/completion";
 
 const SUPPORTED_LANGUAGES = ['java', 'sql'] as const;
-
-/*
-provideCompletionItems: async (model: editor.ITextModel, position: Position) => {
-          const word = model.getWordUntilPosition(position);
-          const range = {
-            startLineNumber: position.lineNumber,
-            endLineNumber: position.lineNumber,
-            startColumn: word.startColumn,
-            endColumn: word.endColumn,
-          };
-
-          const suggestions = new Promise<languages.CompletionItem[]>((resolve, reject) => {
-            resolve([]);
-          });
-          return {
-            suggestions,
-          };
-        }
-      })
-*/
 
 export async function createSQLProposals(
   range: languages.CompletionItem['range']
 ): Promise<languages.CompletionItem[]> {
-  return new Promise<languages.CompletionItem[]>((resolve, reject) => {
-    resolve([
-      {
-        label: 'class',
-        kind: languages.CompletionItemKind.Class,
-        insertText: 'class',
-        detail: '类',
-        range: range
-      },
-      {
-        label: 'public',
-        kind: languages.CompletionItemKind.Variable,
-        insertText: 'public',
-        detail: '权限修饰符',
-        range: range
-      }
-    ])
-  });
+    return (await Keywords(range)).concat(Snippets(range));;
 }
 
 export const EditorExtension: IExtension = {
