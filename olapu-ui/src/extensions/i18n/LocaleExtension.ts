e39f5e21479e9af4e67c8b9d42d026d9b8edc9a0
bnyte <bnytezz@gmail.com>
2023-02-14 19:05:36 +0800

    feat: 编辑器以及Java相关的语法支持

---
+++ olapu-ui/src/extensions/i18n/LocaleExtension.ts
@@ -0,0 +1,13 @@
+import { IExtension } from "@dtinsight/molecule/esm/model";
+import { IExtensionService } from "@dtinsight/molecule/esm/services";
+
+export const LocaleExtension: IExtension = {
+  id: "LocaleExtension",
+  name: "Locale Extension",
+  activate: function (extensionCtx: IExtensionService): void {
+
+  },
+  dispose: function (extensionCtx: IExtensionService): void {
+
+  }
+}
--- olapu-ui/src/pages/index.tsx
+++ olapu-ui/src/pages/index.tsx
@@ -1,27 +1,19 @@
+import { ColorThemeExtensions, EditorExtension, FloderExtension, LocaleExtension } from '@/extensions';
 import molecule, { create, Workbench } from '@dtinsight/molecule';
 import '@dtinsight/molecule/esm/style/mo.css';
-import { Button, Empty } from 'antd';
-import uniqueId from 'lodash';
 
 const moInstance = create({
-  extensions: [],
+  extensions: [
+    ColorThemeExtensions,
+    LocaleExtension,
+    FloderExtension,
+    EditorExtension,
+  ],
+  defaultLocale: 'zh-CN'
 });
 
-// 设置默认系统语言
-molecule.i18n.setCurrentLocale('zh-CN');
-molecule.colorTheme.setTheme('GitHub Plus');
-
-// molecule.folderTree.setEntry(
-//   <div>
-//     <Empty
-//       image={Empty.PRESENTED_IMAGE_SIMPLE}
-//       description={<div style={{ color: 'pink' }}>没有数据</div>} />
-//   </div>
-// );
-
-
-
 export default function HomePage() {
+
   return (
     moInstance.render(<Workbench />)
   );
