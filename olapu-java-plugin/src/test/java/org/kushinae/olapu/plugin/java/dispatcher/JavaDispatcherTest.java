06178b65220633fc2580cbf9ff2d6fd5a92007fb
kaisa <kaisa.liu@mintinglabs.com>
2023-05-26 12:27:16 +0800

    feat: added generate chain supported

--- olapu-java-plugin/src/test/java/org/kushinae/olapu/plugin/java/dispatcher/JavaDispatcherTest.java
+++ olapu-java-plugin/src/test/java/org/kushinae/olapu/plugin/java/dispatcher/JavaDispatcherTest.java
@@ -3,14 +3,7 @@ package org.kushinae.olapu.plugin.java.dispatcher;
 import org.junit.jupiter.api.Test;
 import org.kushinae.olapu.generate.BuildOption;
 import org.kushinae.olapu.generate.Language;
-import org.kushinae.olapu.generate.adapter.HandlerAdapter;
-import org.kushinae.olapu.generate.chain.ExecutionChain;
-import org.kushinae.olapu.generate.dispatcher.Dispatcher;
-import org.kushinae.olapu.generate.mapping.HandlerMapping;
 import org.kushinae.olapu.spi.factory.DefaultGenerateChain;
-import org.kushinae.olapu.spi.factory.dispatcher.DefaultDispatcherFactory;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class JavaDispatcherTest {
 
